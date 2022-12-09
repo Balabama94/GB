4e4408e8d47577addaa4a6b805fa5e220923a17d
Antonio <toni.psk@mail.ru>
2022-12-09 20:34:27 +0400

    Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.

---
+++ HomeWork020/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+void FillMatrix(double[,] matrix)                              // Заполнение массива случайными целыми числами
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = Convert.ToDouble(new Random().Next(-100, 101));
+        }
+    }
+}
+void PrintMatrix(double[,] matrix)                             // Вывод массива на экран
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"[{matrix[i, j]}] ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Задайте количество строк массива");      // Задаем размеры матрицы и заполняем случайными числами
+int rows = Convert.ToInt32(Console.ReadLine());
+while (rows < 2)
+{
+    Console.WriteLine("Количество строк не может быть меньше 2");
+    Console.WriteLine("Повторите ввод значения");
+    rows = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine("Задайте количество столбцов массива");
+int cols = Convert.ToInt32(Console.ReadLine());
+while (cols < 2)
+{
+    Console.WriteLine("Количество столбцов не может быть меньше 2");
+    Console.WriteLine("Повторите ввод значения");
+    cols = Convert.ToInt32(Console.ReadLine());
+}
+double[,] numbers = new double[rows, cols];
+FillMatrix(numbers);
+PrintMatrix(numbers);
